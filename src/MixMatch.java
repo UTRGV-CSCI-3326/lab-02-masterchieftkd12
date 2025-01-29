bdb84268c3b62afa5ac114a6422fef268d263e44
masterchieftkd12 <masterchieftkd12@gmail.com>
2025-01-29 22:34:59 +0000

    hi

---
+++ src/MixMatch.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hello, Java!";
+        char letter = 'A';
+        short smallNumber = 1500;
+        int integerNumber = 100000;
+        long largeNumber = 10000000000L;
+        float decimalNumber = 3.14f;
+        double preciseNumber = 3.14159265359;
+        boolean isJavaFun = true;
+
+        System.out.println("String: A sequence of characters - " + text);
+        System.out.println("char: A single character - " + letter);
+        System.out.println("short: A 16-bit integer - " + smallNumber);
+        System.out.println("int: A 32-bit integer - " + integerNumber);
+        System.out.println("long: A 64-bit integer - " + largeNumber);
+        System.out.println("float: A 32-bit floating-point number - " + decimalNumber);
+        System.out.println("double: A 64-bit floating-point number with more precision - " + preciseNumber);
+        System.out.println("boolean: A true or false value - " + isJavaFun);
+    }
+}
